0c19be427cdc1cf54e5ec8a2a65af47cc67fb081
Sebastián Solorzano <sol21826@uvg.edu.gt>
2024-08-07 23:20:44 -0600

    FINAL
    :)

--- main.c
+++ main.c
@@ -17,13 +17,15 @@
 typedef struct {
     char type;
     int energy;
-    int moved;  // Nueva variable para controlar si la celda ya se movió en este tick
+    int moved;
 } Cell;
 
 Cell grid[GRID_SIZE][GRID_SIZE];
 
+// Funcion para inicializar el tablero
 void initialize_grid() {
-    // Inicializa la cuadrícula vacía
+
+    // Inicializa el tablero con celdas vacias
     for (int i = 0; i < GRID_SIZE; i++) {
         for (int j = 0; j < GRID_SIZE; j++) {
             grid[i][j].type = EMPTY;
@@ -34,7 +36,7 @@ void initialize_grid() {
 
     srand(time(NULL));
 
-    // Inicializa plantas
+    // Agregar poblacion inicial de plantas en el tablero
     for (int k = 0; k < INITIAL_PLANTS; k++) {
         int i, j;
         do {
@@ -42,10 +44,10 @@ void initialize_grid() {
             j = rand() % GRID_SIZE;
         } while (grid[i][j].type != EMPTY);
         grid[i][j].type = PLANT;
-        grid[i][j].energy = 5; // Energía inicial de la planta
+        grid[i][j].energy = 5;
     }
 
-    // Inicializa herbívoros
+    // Agregar poblacion inicial de herbivoros en el tablero
     for (int k = 0; k < INITIAL_HERBIVORES; k++) {
         int i, j;
         do {
@@ -53,10 +55,10 @@ void initialize_grid() {
             j = rand() % GRID_SIZE;
         } while (grid[i][j].type != EMPTY);
         grid[i][j].type = HERBIVORE;
-        grid[i][j].energy = 10; // Energía inicial del herbívoro
+        grid[i][j].energy = 10;
     }
 
-    // Inicializa carnívoros
+    // Agregar poblacion inicial de carnivoros en el tablero
     for (int k = 0; k < INITIAL_CARNIVORES; k++) {
         int i, j;
         do {
@@ -64,10 +66,11 @@ void initialize_grid() {
             j = rand() % GRID_SIZE;
         } while (grid[i][j].type != EMPTY);
         grid[i][j].type = CARNIVORE;
-        grid[i][j].energy = 15; // Energía inicial del carnívoro
+        grid[i][j].energy = 15;
     }
 }
 
+// Funcion para imprimir el tablero
 void print_grid() {
     for (int i = 0; i < GRID_SIZE; i++) {
         for (int j = 0; j < GRID_SIZE; j++) {
@@ -77,12 +80,13 @@ void print_grid() {
     }
 }
 
+// Funcion para actualizar el estado de las plantas y sus caracteristicas
 void update_plant(int i, int j) {
     if (grid[i][j].moved) return;
 
     grid[i][j].energy++;
-    int reproduce = rand() % 100 < 70;
-    if (reproduce && grid[i][j].energy > 17) {
+    int reproduce = rand() % 100 < 30;
+    if (reproduce && grid[i][j].energy > 10) {
         int ni = (i + (rand() % 3) - 1 + GRID_SIZE) % GRID_SIZE;
         int nj = (j + (rand() % 3) - 1 + GRID_SIZE) % GRID_SIZE;
         if (grid[ni][nj].type == EMPTY) {
@@ -95,6 +99,7 @@ void update_plant(int i, int j) {
     grid[i][j].moved = 1;
 }
 
+// Funcion para actualizar el estado de los herbivoros y sus caracteristicas
 void update_herbivore(int i, int j) {
     if (grid[i][j].moved) return;
 
@@ -107,6 +112,7 @@ void update_herbivore(int i, int j) {
     int ni = (i + (rand() % 3) - 1 + GRID_SIZE) % GRID_SIZE;
     int nj = (j + (rand() % 3) - 1 + GRID_SIZE) % GRID_SIZE;
 
+    // Si la celda vecina es una planta, el herbivoro se alimenta
     if (grid[ni][nj].type == PLANT && !grid[ni][nj].moved) {
         grid[i][j].energy += grid[ni][nj].energy;
         grid[ni][nj].type = HERBIVORE;
@@ -122,8 +128,8 @@ void update_herbivore(int i, int j) {
         grid[i][j].moved = 1;
     }
 
-    // Reproducción
-    if (grid[i][j].energy > 1) {
+    // Si el herbivoro tiene suficiente energia, se reproduce
+    if (grid[i][j].energy > 10) {
         int reproduce_i = (i + (rand() % 3) - 1 + GRID_SIZE) % GRID_SIZE;
         int reproduce_j = (j + (rand() % 3) - 1 + GRID_SIZE) % GRID_SIZE;
         if (grid[reproduce_i][reproduce_j].type == EMPTY) {
@@ -135,6 +141,7 @@ void update_herbivore(int i, int j) {
     }
 }
 
+// Funcion para actualizar el estado de los carnivoros y sus caracteristicas
 void update_carnivore(int i, int j) {
     if (grid[i][j].moved) return;
 
@@ -147,6 +154,7 @@ void update_carnivore(int i, int j) {
     int ni = (i + (rand() % 3) - 1 + GRID_SIZE) % GRID_SIZE;
     int nj = (j + (rand() % 3) - 1 + GRID_SIZE) % GRID_SIZE;
 
+    // Si la celda vecina es un herbivoro, el carnivoro se alimenta
     if (grid[ni][nj].type == HERBIVORE && !grid[ni][nj].moved) {
         grid[i][j].energy += grid[ni][nj].energy;
         grid[ni][nj].type = CARNIVORE;
@@ -162,7 +170,7 @@ void update_carnivore(int i, int j) {
         grid[i][j].moved = 1;
     }
 
-    // Reproducción
+    // Si el carnivoro tiene suficiente energia, se reproduce
     if (grid[i][j].energy > 20) {
         int reproduce_i = (i + (rand() % 3) - 1 + GRID_SIZE) % GRID_SIZE;
         int reproduce_j = (j + (rand() % 3) - 1 + GRID_SIZE) % GRID_SIZE;
@@ -175,29 +183,57 @@ void update_carnivore(int i, int j) {
     }
 }
 
+// Funcion para simular un tick del ecosistema
 void simulate_tick() {
-    // Resetear el estado 'moved' de todas las celdas
+
+    // Reiniciar el estado de las celdas por cada tick
+    #pragma omp parallel for collapse(2)
     for (int i = 0; i < GRID_SIZE; i++) {
         for (int j = 0; j < GRID_SIZE; j++) {
             grid[i][j].moved = 0;
         }
     }
 
-    // Actualizar la cuadrícula
-    #pragma omp parallel for collapse(2)
-    for (int i = 0; i < GRID_SIZE; i++) {
-        for (int j = 0; j < GRID_SIZE; j++) {
-            if (grid[i][j].type == PLANT) {
-                update_plant(i, j);
-            } else if (grid[i][j].type == HERBIVORE) {
-                update_herbivore(i, j);
-            } else if (grid[i][j].type == CARNIVORE) {
-                update_carnivore(i, j);
+    // Actualizar el estado de las plantas, herbivoros y carnivoros de forma paralela
+    #pragma omp parallel sections
+    {
+        #pragma omp section
+        {
+            for (int i = 0; i < GRID_SIZE; i++) {
+                for (int j = 0; j < GRID_SIZE; j++) {
+                    if (grid[i][j].type == PLANT) {
+                        update_plant(i, j);
+                    }
+                }
             }
         }
+
+        #pragma omp section
+        {
+            for (int i = 0; i < GRID_SIZE; i++) {
+                for (int j = 0; j < GRID_SIZE; j++) {
+                    if (grid[i][j].type == HERBIVORE) {
+                        update_herbivore(i, j);
+                    }
+                }
+            }
+        }
+
+        #pragma omp section
+        {
+            for (int i = 0; i < GRID_SIZE; i++) {
+                for (int j = 0; j < GRID_SIZE; j++) {
+                    if (grid[i][j].type == CARNIVORE) {
+                        update_carnivore(i, j);
+                    }
+                }
+            }
+        }
+
     }
 }
 
+// Funcion para contar el numero de plantas, herbivoros y carnivoros en el tablero
 void count_species(int *num_plants, int *num_herbivores, int *num_carnivores) {
     *num_plants = 0;
     *num_herbivores = 0;
@@ -218,13 +254,11 @@ void count_species(int *num_plants, int *num_herbivores, int *num_carnivores) {
 
 int main() {
     initialize_grid();
-    print_grid();
+    int num_ticks = 5000;
 
-    int num_ticks = 1500;
     for (int tick = 0; tick < num_ticks; tick++) {
         simulate_tick();
-
-        if ((tick + 1) % 100 == 0) {
+        if ((tick + 1) % 1000 == 0) {
             int num_plants, num_herbivores, num_carnivores;
             count_species(&num_plants, &num_herbivores, &num_carnivores);
 
